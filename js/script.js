e2f3287c5e8c5c2f6a024b1032d0495c8fdcc6e7
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-06-15 09:01:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -163,7 +163,7 @@ const myApp=[
                            
                               {question:"GIGANTE",options:['GANGORRA.jpg','GOTA.jpg','GIGANTE.jpg','GAROTA.jpg'],key:2},
 
-                              {question:"GAFANHOTO",options:['GELADEIRA.jpg','GAFANHOTO.jpg','GELO.jpg','GEMEAS.jpg'],key:2},
+                              {question:"GAFANHOTO",options:['GELADEIRA.jpg','GAFANHOTO.jpg','GELO.jpg','GEMEAS.jpg'],key:1},
                              
                               {question:"G0MA",options:['GULOSEIMAS.jpg','GOLEIRO.jpg','GOTA.jpg','GOMA.jpg'],key:3},
                              
