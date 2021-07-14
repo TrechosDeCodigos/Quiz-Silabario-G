412f4a70d93fa86a15d5904c726c885f4c4059ad
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-07-13 23:54:01 -0300

    Update confetti.js

--- js/confetti.js
+++ js/confetti.js
@@ -1,1 +1,25 @@
 
+ var confetti = {
+            maxCount: 150,
+            speed: 2,
+            frameInterval: 15,
+            alpha: 1,
+            gradient: !1,
+            start: null,
+            stop: null,
+            toggle: null,
+            pause: null,
+            resume: null,
+            togglePause: null,
+            remove: null,
+            isPaused: null,
+            isRunning: null
+        };
+        ! function () {
+            confetti.start = s, confetti.stop = w, confetti.toggle = function () {
+                e ? w() : s()
+            }, confetti.pause = u, confetti.resume = m, confetti.togglePause = function () {
+                i ? m() : u()
+            }, confetti.isPaused = function () {
+                return i
+            }, confetti.remove = function () {
